259266ccc610a71a3e07a1a2c7bd4413fed3ec2f
Nikita <prey122@gmail.com>
2023-01-12 02:25:08 +0300

    button id done with collect jwt token

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
-
-import styles from './login.module.css';
+import { ButtonId } from "../components/button-id/button-id";
+import styles from "./login.module.css";
 
 export const LoginPage: FC = () => {
   return (
     <main className={styles.container}>
       <h1 className={styles.title}>С кем я учусь?</h1>
+      <ButtonId />
     </main>
-  )
-}
+  );
+};
