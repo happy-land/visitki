37a9ad646d088ee98bdf1049850c7864b921200d
Ruslan Kulish <ruslan.s.kulish@gmail.com>
2023-01-16 23:26:57 +0300

    app-routes.tsx HomePage added

--- src/components/app-routes/app-routes.tsx
+++ src/components/app-routes/app-routes.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { LoginPage, DetailPage } from "../../pages";
+import { LoginPage, DetailPage, HomePage } from "../../pages";
 import { Layout } from "../layout/layout";
 import { useState, useEffect } from "react";
 export const AppRoutes = () => {
@@ -17,6 +17,7 @@ export const AppRoutes = () => {
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/profile" element={<div>Страница профиля</div>} />
           <Route path="/detail" element={<DetailPage />} />
