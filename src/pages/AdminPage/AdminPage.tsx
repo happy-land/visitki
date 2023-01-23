3975dc56d04cb0caa9c2fca4a24ef2cf9b3a3366
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2023-01-23 21:11:43 +0300

    fixed

--- src/pages/AdminPage/AdminPage.tsx
+++ src/pages/AdminPage/AdminPage.tsx
@@ -1,42 +1,41 @@
-import {FC, useState} from 'react';
-import styles from './AdminPage.module.css';
-import {AdminCommentsList} from '../../components/admin-comments-list/admin-comments-list'
-import {AdminStudentsList} from '../../components/admin-students-list/admin-students-list'
+import { FC, useState } from "react";
+import styles from "./AdminPage.module.css";
+import { AdminCommentsList } from "../../components/admin-comments-list/admin-comments-list";
+import { AdminStudentsList } from "../../components/admin-students-list/admin-students-list";
 
-export const AdminPage:FC =() =>{
+export const AdminPage: FC = () => {
+  let [category, setCategory] = useState("students");
 
-    let [isUsers, setChange] = useState("students");
-
-  const setUsers = () => {
-    setChange((isUsers = "students"));
+  const setStudentsList = () => {
+    setCategory((category = "students"));
   };
 
-  const setComments = () => {
-    setChange((isUsers = "comments"));
+  const setCommentsList = () => {
+    setCategory((category = "comments"));
   };
-    return(
-        <section className={styles.page}>
-<>
-      <nav className={styles.button_wrapper}>
-        <button
-          className={`${styles.button} ${
-            isUsers === "students" && styles.active
-          }`}
-          onClick={setUsers}
-        >
-          СТУДЕНТЫ
-        </button>
-        <button
-          className={`${styles.button} ${
-            isUsers === "comments" && styles.active
-          }`}
-          onClick={setComments}
-        >
-          КОММЕНТАРИИ
-        </button>
-      </nav>
-      {isUsers === "students" ? <AdminStudentsList /> : <AdminCommentsList />}
-    </>
-        </section>
-    )
-}
+  return (
+    <section className={styles.page}>
+      <>
+        <nav className={styles.button_wrapper}>
+          <button
+            className={`${styles.button} ${
+              category === "students" && styles.active
+            }`}
+            onClick={setStudentsList}
+          >
+            СТУДЕНТЫ
+          </button>
+          <button
+            className={`${styles.button} ${
+              category === "comments" && styles.active
+            }`}
+            onClick={setCommentsList}
+          >
+            КОММЕНТАРИИ
+          </button>
+        </nav>
+        {category === "students" ? <AdminStudentsList /> : <AdminCommentsList />}
+      </>
+    </section>
+  );
+};
